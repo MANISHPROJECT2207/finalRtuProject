83211afdcc2c15104c6b72b1ccca21a204fba63e
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-07-09 14:49:24 +0530

    setup phase 1 done

--- manik/website/views.py
+++ manik/website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item, Subject
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
+from django.contrib import messages
+from manik.settings import BASE_DIR
